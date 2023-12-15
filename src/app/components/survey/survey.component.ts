65a316947db75a7c8e40e5869ec0e07e16873974
Mary Hamlin <mryhmln@gmail.com>
2023-12-15 11:19:38 -0500

    Added fix for custom theme

--- src/app/components/survey/survey.component.ts
+++ src/app/components/survey/survey.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Model } from 'survey-core';
+import { Model, ITheme } from 'survey-core';
 import { SurveyModule } from "survey-angular-ui";
 import { IDocOptions, SurveyPDF } from "survey-pdf";
 import { surveyJson } from '../../../assets/pilot.survey.json';
@@ -9,6 +9,7 @@ import "survey-core/defaultV2.min.css";
 import { Converter } from "showdown";
 import { HeaderComponent } from '../header/header.component';
 import "survey-core/survey.i18n";
+
 
 const pdfDocOptions: IDocOptions = { /* ... */ };
 
@@ -32,8 +33,17 @@ export class SurveyComponent  {
   surveyModel!: Model;
 
   savePdf (sender: { data: any; }) {
+    console.log('savePdf start');
+
     const surveyPdf = new SurveyPDF(surveyJson, pdfDocOptions);
     surveyPdf.mode = "display"; //read-only
+
+    sender.data.locale = this.locale;
+/*
+    sender.data.onTextMarkdown.add(this.doMarkdown);
+    sender.data.applyTheme(themeJson);
+  */
+
     surveyPdf.data = sender.data;
     surveyPdf.save();
   };
@@ -49,12 +59,50 @@ export class SurveyComponent  {
 
   ngOnChanges() {
     const survey = new Model(surveyJson);
+
+    //set the current locale
     survey.locale = this.locale;
+    console.log('ngOnChanges: locale='+ survey.locale);
+
+    //set the markdown renderer
     survey.onTextMarkdown.add(this.doMarkdown);
-    survey.applyTheme(themeJson);
+
+    const theme: ITheme = themeJson as ITheme;
+    survey.applyTheme(theme);
+
     this.surveyModel = survey;
     survey.onComplete.add(this.savePdf);
   }
+
+    /*
+  uploadFiles(_: any, options: { files: any[]; }) {
+      const formData = new FormData();
+      options.files.forEach((file) => {
+          formData.append(file.name, file);
+      });
+                  fetch("https://api.surveyjs.io/private/Surveys/uploadTempFiles", {
+                method: "POST",
+                body: formData
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    options.callback(
+                        "success",
+                        options.files.map((file) => {
+                            return {
+                                file: file,
+                                content: "https://api.surveyjs.io/private/Surveys/getTempFile?name=" + data[file.name]
+                            };
+                        })
+                    );
+                })
+                .catch((error) => {
+                    console.error("Error: ", error);
+                    options.callback("error");
+                });
+  }
+*/
+
 
 }
 
